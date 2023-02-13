c4f240f8926c84e01279962c7b3f0855663e7aa3
karthiksanthamoorthy <karthiksanthamoorthy713@gmail.com>
2023-02-13 11:52:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const homeRouter = require('./routers/homeRouter')
+const port = process.env.port || 5000;
+
+const app = express();
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+//parse application/json
+app.use(bodyParser.json())
+
+app.use('/', homeRouter)
+
+app.listen(port)
